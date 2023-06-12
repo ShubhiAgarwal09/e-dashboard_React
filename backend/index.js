97def95f6433c0ab5e22540826d2a1ef03d17175
shubhi.ag09@gmail.com <shubhi.ag09@gmail.com>
2023-06-12 22:12:26 +0530

    day3

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get("/",(req,resp)=>{
+const connectDB = async()=>{
+    mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema);
+    const data = await product.find();
+    console.warn(data);
+}
+
+/*app.get("/",(req,resp)=>{
     resp.send("app is working");
 });
-
+*/
+connectDB();  
 app.listen(5000);
